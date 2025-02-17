fe269d438a947643ae0684b34b0b10b28fead6a4
ai-student98 <alavimeh493@gmail.com>
2025-02-17 18:47:13 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🤖🤖 Abubakr project')
+st.title('😁😂 My first website')
 
-st.write('Welcome to DS junior course!')
+st.write('Тут я задеплою модель классификации')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+with st.expander('Data'):
+  st.write("X")
+  X_raw = df.drop('species', axis=1)
+  st.dataframe(X_raw)
+
+  st.write("y")
+  y_raw = df.species
+  st.dataframe(y_raw)
